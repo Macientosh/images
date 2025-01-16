9dd8e26fb5cfbc3fb0e7f277576aef765b2c20e8
Arkadiy Ivankov <130675354+Macientosh@users.noreply.github.com>
2025-01-16 13:54:35 +0300

    add spiral

--- images/images.cpp
+++ images/images.cpp
@@ -1,4 +1,4 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 
 void draw_flag(int size) {
     if (size < 5) {
@@ -33,12 +33,56 @@ void draw_flag(int size) {
     printf("\n");
 }
 
+void draw_spiral(int size) {
+    int spiral[size][size];
+
+    // Initialize the grid with spaces
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            spiral[i][j] = ' ';
+        }
+    }
+
+    // Create the spiral
+    int value = '*';
+    int top = 0, bottom = size - 1, left = 0, right = size - 1;
+    while (top <= bottom && left <= right) {
+        for (int i = left; i <= right; i++) {
+            spiral[top][i] = value;
+        }
+        top++;
+
+        for (int i = top; i <= bottom; i++) {
+            spiral[i][right] = value;
+        }
+        right--;
+
+        for (int i = right; i >= left; i--) {
+            spiral[bottom][i] = value;
+        }
+        bottom--;
+
+        for (int i = bottom; i >= top; i--) {
+            spiral[i][left] = value;
+        }
+        left++;
+    }
+
+    // Print the spiral
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf("%c", spiral[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main() {
     int size;
 
     while (1) { // Бесконечный цикл, пока не введено корректное значение
         printf("Enter the size of the flag (an odd number >= 5): ");
-        scanf_s("%d", &size);
+        scanf("%d", &size);
 
         if (size % 2 == 0) {
             printf("Please enter an odd number for the flag to display correctly.\n");
@@ -51,6 +95,11 @@ int main() {
         }
     }
 
+    printf("\nFlag:\n");
     draw_flag(size);
+
+    printf("\nSpiral:\n");
+    draw_spiral(size);
+
     return 0;
 }
